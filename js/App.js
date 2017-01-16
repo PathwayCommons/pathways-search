2b7d1d40c2de44ad7413007616cbcd177e4cf97a
Manfred Cheung <mj3cheun@uwaterloo.ca>
2017-01-16 14:33:59 -0500

    Added default route and basic 404 page

--- js/App.js
+++ js/App.js
@@ -5,6 +5,7 @@ import {Router, Route, Redirect, hashHistory} from 'react-router';
 import {Page} from './components/Page.jsx';
 import {Search} from './search/Search.jsx';
 import {Pathway} from './pathway/Pathway.jsx';
+import {PageNotFound} from './PageNotFound.jsx';
 
 export class App extends React.Component {
 	constructor(props) {
@@ -19,6 +20,7 @@ export class App extends React.Component {
 					<Route path="/search" component={Search}/>
 					<Route path="/pathway" component={Pathway}/>
 				</Route>
+				<Route path="/*" component={PageNotFound}/>
 			</Router>
 		);
 	}
---
+++ js/PageNotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export class PageNotFound extends React.Component {
+render() {
+	return(
+		<div className="PageNotFound">
+			<h1>Page Not Found</h1>
+			<p>Sorry, but the page you were trying to view does not exist.</p>
+		</div>
+	);
+}
+}
