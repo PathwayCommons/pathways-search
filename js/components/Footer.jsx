36ab19103accecdb06a77b2b6a3e7151be9b60c2
Manfred Cheung <mj3cheun@uwaterloo.ca>
2017-01-18 10:40:19 -0500

    Shortened sample footer text to fix mobile display issues

--- js/components/Footer.jsx
+++ js/components/Footer.jsx
@@ -8,9 +8,7 @@ export class Footer extends React.Component {
 	render() {
 		return (
 			<div className="Footer clearfix">
-				Pathway Commons is a collaboration between the Bader Lab at the University of Toronto, the Sander Lab at the cBio Center for Information Biology, Dana-Farber Cancer Institute and the Computational biology collaboratory at Harvard Medical School, and the Demir Lab, Oregon Health & Science University.
-
-				Pathway Commons was originally developed at the Memorial Sloan Kettering Cancer Center and the University of Toronto.
+				University of Toronto - 2017
 			</div>
 		);
 	}
