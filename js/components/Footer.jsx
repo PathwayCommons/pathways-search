8eb2bba5acfa60e2d80796883731c9901577e72d
Manfred Cheung <mj3cheun@uwaterloo.ca>
2017-01-26 10:28:32 -0500

    Added external links to footer

--- js/components/Footer.jsx
+++ js/components/Footer.jsx
@@ -8,7 +8,16 @@ export class Footer extends React.Component {
 	render() {
 		return (
 			<footer className="Footer clearfix">
-				University of Toronto - 2017
+				<a href="//groups.google.com/forum/#!forum/pathway-commons-help">
+					<div className="footerButton">
+						Help
+					</div>
+				</a>
+				<a href="//www.utoronto.ca">
+					<div className="footerText">
+						© University of Toronto 2017
+					</div>
+				</a>
 			</footer>
 		);
 	}
