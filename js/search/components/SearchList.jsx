f2679a12a511bdab1cbba5d3934307a64a5cd289
Manfred Cheung <mj3cheun@uwaterloo.ca>
2017-03-07 15:54:11 -0500

    Added search splash

--- js/search/components/SearchList.jsx
+++ js/search/components/SearchList.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Pagination} from 'react-bootstrap';
+import {Pagination, Modal} from 'react-bootstrap';
 import isEqual from 'lodash/isEqual';
 import isEmpty from 'lodash/isEmpty';
 import classNames from 'classnames';
@@ -21,22 +21,21 @@ export class SearchList extends React.Component {
 
 	// If query prop or dataSource is changed then re-render
 	componentWillReceiveProps(nextProps) {
-		if((!isEqual(this.props.query, nextProps.query) && (!isEmpty(this.props.dataSources))) || (isEmpty(this.props.dataSources) && !isEmpty(nextProps.dataSources))) {
+		if ((!isEqual(this.props.query, nextProps.query) && (!isEmpty(this.props.dataSources))) || (isEmpty(this.props.dataSources) && !isEmpty(nextProps.dataSources))) {
 			this.getSearchResult(nextProps.query);
 		}
 	}
 
 	getSearchResult(query) {
-		if(!isEmpty(query)) {
-			httpGetAsync(getSearchQueryURL(query), (responseText) =>
-				this.updateSearchResult(responseText));
+		if (!isEmpty(query)) {
+			httpGetAsync(getSearchQueryURL(query), (responseText) => this.updateSearchResult(responseText));
 		}
 	}
 
 	// Handle JSON string returned from search request
 	updateSearchResult(searchResultObj) {
 		var searchData = parseJSON(searchResultObj);
-		if(searchData) {
+		if (searchData) {
 			// Process searchData to add extra properties from dataSources
 			searchData = {
 				searchHit: searchData.searchHit.map((searchResult) => {
@@ -56,50 +55,74 @@ export class SearchList extends React.Component {
 			...this.props.query,
 			page: (e - 1).toString()
 		});
-		scroll(0,0);
+		scroll(0, 0);
 	}
 
 	render() {
 		var searchData = this.state.searchResult;
 
 		// Only load populated searchlist if searchData is populated
-		if(!isEmpty(searchData)) {
+		if (!isEmpty(searchData)) {
 			var hitList = searchData.searchHit;
 			return (
 				<div className="SearchList">
 					{hitList.map((item, index) => {
-						if(item.size > 0) {
-							return(<SearchItem key={index} dataSources={this.props.dataSources} data={item}/>);
+						if (item.size > 0) {
+							return (<SearchItem key={index} dataSources={this.props.dataSources} data={item}/>);
 						}
 					})}
-					<div className={classNames("paginationContainer", "text-center", hitList.length == 0 ? "hidden" : "")}>
-						<Pagination
-							first
-							last
-							ellipsis
-							boundaryLinks
-							items={Math.ceil(searchData.numHits / searchData.maxHitsPerPage)}
-							maxButtons={3}
-							activePage={parseInt(this.props.query.page) + 1 || 1}
-							onSelect={(e) => this.handleSelect(e)}
-						/>
+					<div className={classNames("paginationContainer", "text-center", hitList.length == 0
+						? "hidden"
+						: "")}>
+						<Pagination first last ellipsis boundaryLinks items={Math.ceil(searchData.numHits / searchData.maxHitsPerPage)} maxButtons={3} activePage={parseInt(this.props.query.page) + 1 || 1} onSelect={(e) => this.handleSelect(e)}/>
 					</div>
 				</div>
+			// If searchData is null this indicates no search results were found
 			);
-		}
-		// If searchData is null this indicates no search results were found
-		else if(searchData === null) {
+		} else if (searchData === null) {
 			return (
 				<div className="SearchList">
 					<div className="noSearchResults">
 						No Search Results Found
 					</div>
 				</div>
+			// Otherwise assume page is still loading and display nothing
 			);
-		}
-		// Otherwise assume page is still loading and display nothing
-		else {
-			return null;
+		} else {
+			return (
+				<div className="SearchList">
+					<Modal.Dialog>
+						<Modal.Header>
+							<Modal.Title>Search</Modal.Title>
+						</Modal.Header>
+
+						<Modal.Body>
+							<div className="text">
+								Find and view pathways within Pathway Commons*.
+							</div>
+							<div className="text">
+								Search by pathway name or a list of genes.
+							</div>
+							<div className="text">
+								View pathways directly by typing its URI.
+							</div>
+							<br/>
+							<div className="subtext">
+								*Pathway Commons houses human metabolic, signalling and gene regulatory pathways along with genetic and molecular interactions.
+							</div>
+						</Modal.Body>
+						<Modal.Footer>
+							<a href="http://www.pathwaycommons.org/">
+								Pathway Commons
+							</a>
+							{"  |  "}
+							<a href="http://groups.google.com/forum/#!forum/pathway-commons-help">
+								Help
+							</a>
+						</Modal.Footer>
+					</Modal.Dialog>
+				</div>
+			);
 		}
 	}
 }
