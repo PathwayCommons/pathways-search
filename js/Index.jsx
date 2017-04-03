67e36df449abec88e90b16c721d79db59194f043
Manfred Cheung <mj3cheun@uwaterloo.ca>
2017-04-03 15:14:25 -0400

    Added PC Offline error message

--- js/Index.jsx
+++ js/Index.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import {Switch, Route, Redirect} from 'react-router-dom';
 import classNames from 'classnames';
 import queryString from 'query-string';
+import {utilities} from 'pathway-commons';
 
 import {Header} from './components/Header.jsx';
 import {Faq} from './faq/Faq.jsx';
 import {Search} from './search/Search.jsx';
 import {Pathway} from './pathway/Pathway.jsx';
 import {PageNotFound} from './PageNotFound.jsx';
+import {ErrorMessage} from './components/ErrorMessage.jsx';
 
 // Index
 // Prop Dependencies ::
@@ -18,15 +20,22 @@ export class Index extends React.Component {
 		super(props);
 		this.state = {
 			help: false,
+			pcOnline: true,
 			...this.handlePropUpdates(this.props)
 		};
+		this.pcLiveCheck();
 	}
 
 	// If location.search is changed then re-render
 	componentWillReceiveProps(nextProps) {
 		if (this.props !== nextProps) {
 			this.setState(this.handlePropUpdates(nextProps));
+			this.pcLiveCheck();
 		}
+	}
+
+	pcLiveCheck() {
+		utilities.pcCheck(10000).then(isOnline => this.setState({pcOnline: isOnline}));
 	}
 
 	handlePropUpdates(props) {
@@ -50,25 +59,34 @@ export class Index extends React.Component {
 		return this.state;
 	}
 
-		render() {
-			var globalObject = Object.assign({
-				updateGlobal: (key, value) => this.updateGlobal(key, value),
-				deleteGlobal: (key) => this.deleteGlobal(key)
-			}, this.getAllGlobals());
+	render() {
+		var globalObject = Object.assign({
+			updateGlobal: (key, value) => this.updateGlobal(key, value),
+			deleteGlobal: (key) => this.deleteGlobal(key)
+		}, this.getAllGlobals());
 
-			return (
-				<div className={classNames("Index", this.state.embed ? "iframe" : "", this.props.className)}>
-					<Header hidden={this.state.embed} {...globalObject}/>
-					<div className={this.state.embed ? "" : "Content"}>
-						<Switch>
-							<Route exact path="/" render={() => <Redirect to="/search"/>}/>
-							<Route path="/search" render={props => <Search {...props} {...globalObject}/>}/>
-							<Route path="/faq" render={props => <Faq {...props} {...globalObject}/>}/>
-							<Route path="/pathway" render={props => <Pathway {...props} {...globalObject}/>}/>
-							<Route path="*" render={props => <PageNotFound {...props} {...globalObject}/>}/>
-						</Switch>
-					</div>
+		return (
+			<div className={classNames("Index", this.state.embed ? "iframe" : "", this.props.className)}>
+				<Header hidden={this.state.embed} {...globalObject}/>
+				<div className={this.state.embed ? "" : "Content"}>
+					{
+						this.state.pcOnline ?
+						(
+							<Switch>
+								<Route exact path="/" render={() => <Redirect to="/search"/>}/>
+								<Route path="/search" render={props => <Search {...props} {...globalObject}/>}/>
+								<Route path="/faq" render={props => <Faq {...props} {...globalObject}/>}/>
+								<Route path="/pathway" render={props => <Pathway {...props} {...globalObject}/>}/>
+								<Route path="*" render={props => <PageNotFound {...props} {...globalObject}/>}/>
+							</Switch>
+						) : (
+							<ErrorMessage>
+								Pathway Commons Offline : Please try again later
+							</ErrorMessage>
+						)
+					}
 				</div>
-			);
-		}
+			</div>
+		);
 	}
+}
