5680340d5c14643b77528381f914d37816557caa
Manfred Cheung <mj3cheun@uwaterloo.ca>
2017-04-05 11:39:02 -0400

    Changed offline message to alert

--- js/Index.jsx
+++ js/Index.jsx
@@ -3,13 +3,13 @@ import {Switch, Route, Redirect} from 'react-router-dom';
 import classNames from 'classnames';
 import queryString from 'query-string';
 import {utilities} from 'pathway-commons';
+import {Alert} from 'react-bootstrap';
 
 import {Header} from './components/Header.jsx';
 import {Faq} from './faq/Faq.jsx';
 import {Search} from './search/Search.jsx';
 import {Pathway} from './pathway/Pathway.jsx';
 import {PageNotFound} from './PageNotFound.jsx';
-import {ErrorMessage} from './components/ErrorMessage.jsx';
 
 // Index
 // Prop Dependencies ::
@@ -35,7 +35,7 @@ export class Index extends React.Component {
 	}
 
 	pcLiveCheck() {
-		utilities.pcCheck(10000).then(isOnline => this.setState({pcOnline: isOnline}));
+		utilities.pcCheck(5000).then(isOnline => this.setState({pcOnline: isOnline}));
 	}
 
 	handlePropUpdates(props) {
@@ -70,21 +70,19 @@ export class Index extends React.Component {
 				<Header hidden={this.state.embed} {...globalObject}/>
 				<div className={this.state.embed ? "" : "Content"}>
 					{
-						this.state.pcOnline ?
-						(
-							<Switch>
-								<Route exact path="/" render={() => <Redirect to="/search"/>}/>
-								<Route path="/search" render={props => <Search {...props} {...globalObject}/>}/>
-								<Route path="/faq" render={props => <Faq {...props} {...globalObject}/>}/>
-								<Route path="/pathway" render={props => <Pathway {...props} {...globalObject}/>}/>
-								<Route path="*" render={props => <PageNotFound {...props} {...globalObject}/>}/>
-							</Switch>
-						) : (
-							<ErrorMessage>
-								Pathway Commons Offline : Please try again later
-							</ErrorMessage>
+						this.state.pcOnline ? null : (
+							<Alert bsStyle="warning">
+								<strong>Pathway Commons Offline :</strong> Please try again later
+							</Alert>
 						)
 					}
+					<Switch>
+						<Route exact path="/" render={() => <Redirect to="/search"/>}/>
+						<Route path="/search" render={props => <Search {...props} {...globalObject}/>}/>
+						<Route path="/faq" render={props => <Faq {...props} {...globalObject}/>}/>
+						<Route path="/pathway" render={props => <Pathway {...props} {...globalObject}/>}/>
+						<Route path="*" render={props => <PageNotFound {...props} {...globalObject}/>}/>
+					</Switch>
 				</div>
 			</div>
 		);
