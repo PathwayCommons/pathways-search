ffc5db1c551a4318e9a305f2b71f94bab55a3639
Jeffrey Wong <jvwong@outlook.com>
2017-05-26 14:26:35 -0400

    Branded and responsive pathway

--- js/pathway/Pathway.jsx
+++ js/pathway/Pathway.jsx
@@ -43,49 +43,55 @@ export class Pathway extends React.Component {
 
 	render() {
 		const tip_screenshot = (
-			<Popover className="info-tip" id="popover-screenshot" placement="bottom" title="Screenshot">
+			<Popover className="info-tip hidden-xs" id="popover-screenshot" placement="bottom" title="Screenshot">
 				Click to download an image (png) of the current view.
 			</Popover>
 		);
 		const tip_downloads = (
-			<Popover className="info-tip" id="popover-downloads" placement="bottom" title="Downloads">
+			<Popover className="info-tip hidden-xs" id="popover-downloads" placement="bottom" title="Downloads">
 				Download this pathway in several different formats.
 			</Popover>
 		);
 		const tip_metadata = (
-			<Popover className="info-tip" id="popover-metadata" placement="bottom" title="Info">
+			<Popover className="info-tip hidden-xs" id="popover-metadata" placement="bottom" title="Info">
 				Click to see any description and information provided by the original datasource.
 			</Popover>
 		);
 		if(this.state.pathwayData) {
 			return(
 				<div className="Pathway">
-					<Navbar collapseOnSelect>
-						<Navbar.Header>
-							<Navbar.Toggle />
-						</Navbar.Header>
-						<Navbar.Collapse>
-							<Nav>
-								<NavItem eventKey={1} onClick={() => this.setState({active: "Information"})}>
-									<OverlayTrigger delayShow={1000} placement="bottom" overlay={tip_metadata}>
-										<span id="metadata">{this.state.name ? this.state.name : this.props.query.uri} | {this.state.datasource}</span>
-									</OverlayTrigger>
-								</NavItem>
-							</Nav>
-							<Nav pullRight>
-								<NavItem eventKey={1} onClick={() => this.props.graphImage(false)}>
-									<OverlayTrigger delayShow={1000} placement="bottom" overlay={tip_screenshot}>
-										<span className="navitem-label">Screenshot</span>
-									</OverlayTrigger>
-								</NavItem>
-								<NavItem eventKey={2} onClick={() => this.setState({active: "Downloads"})}>
-									<OverlayTrigger delayShow={1000} placement="bottom" overlay={tip_downloads}>
-										<span className="navitem-label">Downloads</span>
-									</OverlayTrigger>
-								</NavItem>
-							</Nav>
-						</Navbar.Collapse>
-					</Navbar>
+					{ !this.props.embed &&
+						(<Navbar collapseOnSelect>
+							<Navbar.Header>
+								<Col xsOffset={1} xs={9} smOffset={0} sm={2}>
+									<span className="brand">View</span>
+								</Col>
+
+					      <Navbar.Toggle />
+							</Navbar.Header>
+							<Navbar.Collapse>
+								<Nav>
+									<NavItem eventKey={1} onClick={() => this.setState({active: "Information"})}>
+										<OverlayTrigger delayShow={1000} placement="bottom" overlay={tip_metadata}>
+											<div id="metadata">{this.state.name ? this.state.name : this.props.query.uri} | {this.state.datasource}</div>
+										</OverlayTrigger>
+									</NavItem>
+								</Nav>
+								<Nav pullRight>
+									<NavItem eventKey={1} onClick={() => this.props.graphImage(false)}>
+										<OverlayTrigger delayShow={1000} placement="bottom" overlay={tip_screenshot}>
+											<span className="navitem-label">Screenshot</span>
+										</OverlayTrigger>
+									</NavItem>
+									<NavItem eventKey={2} onClick={() => this.setState({active: "Downloads"})}>
+										<OverlayTrigger delayShow={1000} placement="bottom" overlay={tip_downloads}>
+											<span className="navitem-label">Downloads</span>
+										</OverlayTrigger>
+									</NavItem>
+								</Nav>
+							</Navbar.Collapse>
+						</Navbar>)
+					}
 					<Graph pathwayData={this.state.pathwayData} {...this.props}/>
 					{/* Menu Modal */}
 					<ModalFramework onHide={() => this.setState({active: ""})} {...this.state} {...this.props}/>
