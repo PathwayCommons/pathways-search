5580e542479a46ab4a58d198f4712a00fd1f1df3
d2fong <d2fong@uwaterloo.ca>
2017-06-29 10:32:14 -0400

    upgrade to cose-bilkent 1.6.12; use recommended settings

--- js/view/components/graph/layout/coseBilkent.js
+++ js/view/components/graph/layout/coseBilkent.js
@@ -1,24 +1,11 @@
 const coseBilkentLayout = (cy) => {
 	cy.layout({
 		name: 'cose-bilkent',
-		paddingCompound: 90,
 		fit: true,
-		nodeRepulsion: 4500,
-		idealEdgeLength: 50,
-		edgeElasticity: 0.45,
-		nestingFactor: 0.1,
-		gravity: 0.25,
-		numIter: 2500,
-		tile: false,
 		animationEasing: 'cubic-bezier(0.19, 1, 0.22, 1)',
-		animate: 'end',
 		animationDuration: 1000,
-		randomize: true,
 		tilingPaddingVertical: 20,
-		tilingPaddingHorizontal: 20,
-		gravityRangeCompound: 1.5,
-		gravityCompound: 1.0,
-		gravityRange: 3.8,
+		tilingPaddingHorizontal: 20
 	}).run();
 };
 
