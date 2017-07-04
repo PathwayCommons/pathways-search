98942006572c89852377cdea146bfee1c9736272
Dylan Fong <d2fong@uwaterloo.ca>
2017-07-04 10:25:17 -0400

    set larger spacing and border spacing values to avoid node overlap in klay

--- js/view/components/graph/layout/klay.js
+++ js/view/components/graph/layout/klay.js
@@ -2,12 +2,12 @@ const klayLayout = (cy) => {
 	cy.layout({
 		name: 'klay',
 		klay: {
-			borderSpacing: 20,
+			borderSpacing: 50,
 			separateConnectedComponents: true,
 			aspectRatio: cy.width() / cy.height(),
 			thoroughness: 7,
 			compactComponents: false,
-			spacing: 20,
+			spacing: 75,
 			edgeSpacingFactor: 0.5,
 			layoutHierarchy: true
 		},
