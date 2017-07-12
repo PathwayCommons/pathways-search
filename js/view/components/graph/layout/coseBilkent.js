fbeb89d7447d717f86fecc4acb25d726537b14b2
Dylan Fong <d2fong@uwaterloo.ca>
2017-07-12 11:44:03 -0400

    reduce randomness in fd layouts

--- js/view/components/graph/layout/coseBilkent.js
+++ js/view/components/graph/layout/coseBilkent.js
@@ -1,12 +1,13 @@
 const coseBilkentLayout = (cy) => {
-	cy.layout({
-		name: 'cose-bilkent',
-		fit: true,
-		animationEasing: 'cubic-bezier(0.19, 1, 0.22, 1)',
-		animationDuration: 1000,
-		tilingPaddingVertical: 20,
-		tilingPaddingHorizontal: 20
-	}).run();
+  cy.layout({
+    name: 'cose-bilkent',
+    fit: true,
+    randomize: false,
+    animationEasing: 'cubic-bezier(0.19, 1, 0.22, 1)',
+    animationDuration: 1000,
+    tilingPaddingVertical: 20,
+    tilingPaddingHorizontal: 20
+  }).run();
 };
 
 export default coseBilkentLayout;
