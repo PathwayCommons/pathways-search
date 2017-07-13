be9ddfbfe83ef489300bf007a5943eb23fe0d199
Dylan Fong <d2fong@uwaterloo.ca>
2017-07-13 17:59:28 -0400

    style changes - increase font size - wrap long complex node labels - place labbel inside node body when collapsed

--- js/view/components/graph/stylesheet.js
+++ js/view/components/graph/stylesheet.js
@@ -2,12 +2,32 @@ import cytoscape from 'cytoscape';
 import sbgnStyleSheet from 'cytoscape-sbgn-stylesheet';
 
 const stylesheet = sbgnStyleSheet(cytoscape)
+.selector('node').css({
+  'text-wrap': 'wrap',
+  'text-max-width': 150
+})
 .selector('node[class!="compartment"]')
 .css({
-  'font-size': 16,
+  'font-size': 20,
   'color': 'black',
   'text-outline-color': 'white',
-  'text-outline-width': 2
+  'text-outline-width': 2,
+  'text-outline-opacity': 0.5
+})
+.selector('node[class="complex"]')
+.css({
+  'content': (node) => node.data('label').split(':').join(' '),
+  'width': 30,
+  'height': 30
+})
+.selector('.cy-expand-collapse-collapsed-node')
+.css({
+  'background-color': '#3f3c3c',
+  'color': 'white',
+  'text-outline-color': 'black',
+  'text-outline-width': 2,
+  'text-outline-opacity': 1,
+  'text-valign': 'center'
 })
 .selector('edge')
 .css({
