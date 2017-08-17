978116188df0d40d34d4c2001e2ca9ff2c9773c7
d2fong <d2fong@uwaterloo.ca>
2017-08-17 11:08:46 -0400

    remove uglify -- not working, causes errors

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -8,11 +8,6 @@ module.exports = {
 			'process.env': {
 				'NODE_ENV': JSON.stringify('production')
 			}
-		}),
-		new webpack.optimize.UglifyJsPlugin({
-			compress: {
-				warnings: true
-			}
 		})
 	],
 	entry: ['./js/App.js'],
