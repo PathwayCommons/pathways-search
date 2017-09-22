9cf8950e718b1353f9e87faf0082c19eb0d99b86
Dylan Fong <d2fong@uwaterloo.ca>
2017-09-22 15:50:53 -0400

    Config indentation

--- webpack.config.js
+++ webpack.config.js
@@ -2,30 +2,30 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-	entry: ['./src/client/index.js'],
-	output: {
-		path: __dirname + '/public',
-		filename: 'bundle.js'
-	},
-	devtool: 'source-map',
-	module: {
-		rules: [{
-				test: /.jsx?$/,
-				use: 'babel-loader',
-				exclude: /node_modules/
-			},
-			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader', 'postcss-loader']
-			},
-			{
-				test: /\.(eot|svg|ttf|woff|woff2)$/,
-				use: 'file-loader?name=fonts/[name].[ext]'
-			},
-			{
-				test: /\.png$/,
-				use: 'url-loader'
-			}
-		]
-	}
+  entry: ['./src/client/index.js'],
+  output: {
+    path: __dirname + '/public',
+    filename: 'bundle.js'
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [{
+        test: /.jsx?$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader']
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        use: 'file-loader?name=fonts/[name].[ext]'
+      },
+      {
+        test: /\.png$/,
+        use: 'url-loader'
+      }
+    ]
+  }
 };
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -3,42 +3,42 @@ var webpack = require('webpack');
 var webpackStripLoader = require('strip-loader');
 
 module.exports = {
-	plugins: [
-		new webpack.DefinePlugin({
-			'process.env': {
-				'NODE_ENV': JSON.stringify('production')
-			}
-		})
-	],
-	entry: ['./js/App.js'],
-	output: {
-		path: __dirname + '/public',
-		filename: 'bundle.js'
-	},
-	devtool: 'source-map',
-	module: {
-		rules: [{
-				test: /.jsx?$/,
-				use: 'babel-loader',
-				exclude: /node_modules/
-			},
-			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader', 'postcss-loader']
-			},
-			{
-				test: /\.(eot|svg|ttf|woff|woff2)$/,
-				use: 'file-loader?name=fonts/[name].[ext]'
-			},
-			{
-				test: /\.png$/,
-				use: 'url-loader'
-			},
-			{
-				test: [/\.js$/, /\.jsx$/],
-				exclude: /node_modules/,
-				use: webpackStripLoader.loader('console.log')
-			}
-		]
-	}
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    })
+  ],
+  entry: ['./js/App.js'],
+  output: {
+    path: __dirname + '/public',
+    filename: 'bundle.js'
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [{
+        test: /.jsx?$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader']
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        use: 'file-loader?name=fonts/[name].[ext]'
+      },
+      {
+        test: /\.png$/,
+        use: 'url-loader'
+      },
+      {
+        test: [/\.js$/, /\.jsx$/],
+        exclude: /node_modules/,
+        use: webpackStripLoader.loader('console.log')
+      }
+    ]
+  }
 };
